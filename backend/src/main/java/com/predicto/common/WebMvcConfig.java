b7cef9b1af1fba0b724c7d2cc954d5bbb941ba76
Predicto Dev <dev@predicto.local>
2026-06-24 18:12:27 +0200

    Remove content negotiation config, add asset verification in build

--- backend/src/main/java/com/predicto/common/WebMvcConfig.java
+++ backend/src/main/java/com/predicto/common/WebMvcConfig.java
@@ -1,17 +1,9 @@
 package com.predicto.common;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.mediaType("css", MediaType.valueOf("text/css"))
-                  .mediaType("js", MediaType.valueOf("application/javascript"))
-                  .mediaType("svg", MediaType.valueOf("image/svg+xml"));
-    }
 }
