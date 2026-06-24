6b81dc60a00015db440ac2e9bb913bdee69eab62
Predicto Dev <dev@predicto.local>
2026-06-24 18:16:56 +0200

    Fix SpaController to not intercept assets, add static resource config

--- backend/src/main/java/com/predicto/config/SpaController.java
+++ backend/src/main/java/com/predicto/config/SpaController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SpaController {
 
-    @GetMapping(value = {"/{path:[^\\.]*}", "/"})
+    @GetMapping(value = {
+        "/",
+        "/{path:^(?!api|assets|actuator).*$}/**"
+    })
     public String forward() {
         return "forward:/index.html";
     }
